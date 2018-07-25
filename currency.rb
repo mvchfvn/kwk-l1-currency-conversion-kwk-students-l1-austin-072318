79392b09e1efdc2f53f7e16dff76115675ede508
mvchfvn <mimi123meg@gmail.com>
2018-07-25 14:21:57 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -49,7 +49,22 @@ case choice
 when 1 
   puts usd_to_eur(amount)
 when 2 
-  puts usd_to_eur(amount)  
+  puts usd_to_jpy(amount) 
+when 3
+  puts usd_to_gbp(amount)
+when 4 
+  puts usd_to_aud(amount)
+when 5
+  puts eur_to_usd(amount)
+when 6 
+  puts jpy_to_usd(amount)
+when 7 
+  puts gbp_to_usd(amount)
+when 8
+  puts aud_to_usd(amount)
+else
+  puts "Invalid input, exiting..."
+end
   
   
   

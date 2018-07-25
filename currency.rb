7edc92aafc8bb39cfdfbf607877b5918da843ef4
mvchfvn <mimi123meg@gmail.com>
2018-07-25 14:13:43 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,1 +1,28 @@
 # Write your cod here
+def usd_to_eur(dollar)
+  return dollar*0.86
+end
+
+def eur_to_usd(euros)
+  return euros*1.17
+end
+
+def usd_to_jpy(dollars)
+  return dollars*111.1
+end
+
+def usd_to_gbp(dollars)
+  return
+end
+
+def usd_to_aud(dollars)
+end
+
+def jpy_to_usd(jp)
+end
+
+def gbp_to_usd(gbp)
+end
+
+def aud_to_usd(aud)
+end

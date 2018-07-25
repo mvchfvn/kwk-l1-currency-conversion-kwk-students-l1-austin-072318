1f371205f9134bb1be611f57c4aaf00469c56f14
mvchfvn <mimi123meg@gmail.com>
2018-07-25 14:19:54 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -34,4 +34,28 @@ end
 puts "How much money do you want to convert?"
 amount = gets.chomp().to_i
 
-puts 
+puts "Pick from the following menu:"
+puts "1. USD to EUR"
+puts "2. USD to JPY"
+puts "3. USD to GBP"
+puts "4. USD to AUD"
+puts "5. EUR to USD"
+puts "6. JPY to USD"
+puts "7. GBP to USD"
+puts "8. AUD to USD"
+
+choice = gets.chomp().to_i
+case choice
+when 1 
+  puts usd_to_eur(amount)
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
